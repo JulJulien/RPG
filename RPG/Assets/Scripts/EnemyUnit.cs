fd5a4cd353d753d14f3d4fbce2a33ee43b798b17
Carlos Garza <cjulgarza@gmail.com>
2024-09-18 19:12:15 -0500

    Enemy Instantiation Working

---
+++ RPG/Assets/Scripts/EnemyUnit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUnit : MonoBehaviour
+{
+    public string EnemyName;
+    public int damage;
+    public string attackName;
+    public int maxHP;
+    public int currentHP;
+    public string damageType;
+    public string resistances;
+}
